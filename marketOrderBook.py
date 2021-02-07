9115b8d81c30e564f2162b904d3e4f4360c84030
Ignacio <nacho@Ignacios-iMac.local>
2021-02-07 12:17:14 -0300

    added marketOrderBook

---
+++ marketOrderBook.py
@@ -0,0 +1,58 @@
+import json
+import hmac
+import requests
+import time
+from hashlib import sha512
+
+def hmac_sha512(secret_key, timestamp, body):
+    # para usar hmac es necesario convertir el secret key 
+    # de string utf-8 a bytearray
+    key = bytearray(secret_key, 'utf-8')
+    msg = str(timestamp) + str(body)
+
+    # ademas sha512 requiere de strings codificados
+    msg = msg.encode('utf-8')
+
+    return hmac.HMAC(key, msg, sha512).hexdigest()
+
+def orderBook(api_key, secret_key, marketCode):
+    query_str = '''
+      query marketOrderBook($marketCode: ID!, $limit: Int) {
+          marketOrderBook(marketCode: $marketCode, limit: $limit) {
+            sell {
+              amount
+              limitPrice
+              accumulated
+            }
+            buy {
+              amount
+              limitPrice
+              accumulated
+            }
+          }
+        }
+    '''
+
+    variables = {
+      'marketCode': marketCode,
+      'limit': 15
+    }
+    query = {
+      'query': query_str,
+      'variables': variables
+    }
+    body = json.dumps(query)
+    timestamp = str(time.time())
+    signature = str(hmac_sha512(secret_key, timestamp, body))
+    headers = {
+      'Content-Type': 'application/json',
+      'X-ORIONX-TIMESTAMP': timestamp, 
+      'X-ORIONX-APIKEY': api_key, 
+      'X-ORIONX-SIGNATURE': signature, 
+    }
+    url = 'https://api2.orionx.com/graphql'
+    response = requests.post(url=url, headers=headers, data=body)
+    response.raise_for_status()
+    data = json.loads(response.text)
+
+    return data['data']
