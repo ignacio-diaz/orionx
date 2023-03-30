0a1f98974382960c40d81d160955a7618f8a57cf
ignacio-diaz <ignacio.diaz@ies.cl>
2023-03-30 13:12:30 -0300

    feat(orionx_py): get_balance

---
+++ balance.py
@@ -0,0 +1,52 @@
+import json, time, requests
+from new_position import hmac_sha512 
+
+def get_balance(api_key, secret_key):
+
+    query_str = '''
+    query{
+        me {
+            wallets {
+                currency {
+                    code
+                    isCrypto
+                    units
+                }
+                balance
+                }
+        }
+    }'''
+    query = {
+    'query': query_str
+    }
+
+
+    body = json.dumps(query)
+    timestamp = str(time.time())
+    signature = str(hmac_sha512(secret_key, timestamp, body))
+    headers = {
+        'Content-Type': 'application/json',
+        'X-ORIONX-TIMESTAMP': timestamp,
+        'X-ORIONX-APIKEY': api_key,
+        'X-ORIONX-SIGNATURE': signature,
+    }
+
+    response = requests.post('https://api2.orionx.com/graphql', headers=headers, data=body)
+    data = json.loads(response.text)
+    data = data['data']["me"]["wallets"]
+
+    new_balance = {}
+
+    for asset in data:
+        new_balance[asset["currency"]["code"]] = asset["balance"] / 10 ** asset["currency"]["units"]
+
+    return new_balance
+
+
+if __name__ == "__main__":
+    api_key = input("Ingresa tu API_KEY : ")
+    secret_key = input("Ingresa tu SECRET_KEY : ")
+    
+    balance = get_balance(api_key, secret_key)
+    print(balance)
+
